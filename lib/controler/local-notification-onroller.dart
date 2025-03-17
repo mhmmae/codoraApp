c2f74f568158ba7d9e047d52cd8d5266bb18323e
mostafaaltlal <mhmmae19@gmail.com>
2025-03-17 16:38:57 +0300

    first commit

--- lib/controler/local-notification-onroller.dart
+++ lib/controler/local-notification-onroller.dart
@@ -2,6 +2,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:http/http.dart' as http;
 import 'package:googleapis_auth/auth_io.dart' as auth;
@@ -701,16 +702,13 @@ List<Message>? mes =[];
 
     final serviceAccontejeson = {
       "type": "service_account",
-      "project_id": "homy-3693e",
-      "private_key_id": "d166e88626af8dbd296223b6184e8c81aad933c6",
-      "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEuwIBADANBgkqhkiG9w0BAQEFAASCBKUwggShAgEAAoIBAQDDaCc89TomXu1Q\nCBGwVHdIn3mq7YAVkM8CIx1RBo0E04iykXwupvnxxeuvTRZVEuCcZj+lpXlxQQwW\nF5tGGXhM6W1XniU7pWxPyLbrsRIx6yYwTTJkyssaY6Q9xiLibwe81kRt2Jal6D0K\n1skfqXLCSOPwJE/3SUXtrRcg7aYbgBZmNuyjC6xxAjsdig9b7AlS44pvZeE4PC74\nK2WBr69Zmo93Sz8zP7BVH3uKam5pMvkrkCafJJrhK+YmNmEWW423Rneu6Cg7bGsi\n4WTW6ot3T3gNG4qcO9f4qSLTw5LUZCPGQijwBzNSLB6Y4v+sS1EQhkY5OpaWL4z4\nD2Js8N6DAgMBAAECgf9noVTpoUYtAjv7CvcjYeyLfaGIyQVxWWNeYOurSKNv9Fjo\nfNkjOgQx+M2WZjHguRmp2Cm1ySW4SEb1+PVolaHqW1i7SYF1fl8MV1139vBiiOPz\nl6fVVOpyx4dbf8FVuzcjkMMO9dcRR7sXyL/hJFonBJIiFey5L/gyx3+FIbbciIDW\nd80aPae5qO52tniJlV1giQOymzliXZo0jP5sGqag5E1a5HZM/Icb/iZxcFBxZkZB\n+Wx4uyQ2iufHDJAPgXeamW0WXq5iWnpdu44qg0CUckD/yYsiecTW6dL5xF0RT0Ps\nrgpIVx5dOoFqe/qNtp1mrN41k9np8A2iuJlcqc0CgYEA+BiRJklU/ic09BLhB3vZ\nPEXRG2JE1kUaSMZ+RJImHoIGTqmM8v5j3tRMIjxXwoKBDbHYaTU+vA8QqoqC/3oS\n0UYB8Rv1Q8fAHO5ppSQzIrmBB2/jbuFHsoO5mmmXHDDp8Z0QU6aOTx8At2tVcLnF\nYpOJ33zY5LuqoVOYr78P8g0CgYEAyaHcooRf1cLUBYEeD4Ly3hiC5MaRolE+FSMe\ntKDJWWg/uJNtDzBuJiyQXHFat/DJ1X6nbuL5pPnL8MZ2txKB0SpuxOLnw4psZHsY\nK/8EbqnifTmFBdDqhehN4X9/WvsVAOH1v9y161tOeCCPxK2Wpa1mvnHgu2xr95Jp\nDmnhPs8CgYA5o6uFU6A1c/JvijtRu8paoHXWgNwxU9ipc8Q+Nh45FEhW2jlu8v9M\n89HEWnShMiS9g8Ydm0s58d4TYR7SMBBTIoqs86vl1XCiyBkvTtu6g5KgobQbPKel\np8jlQQbke9C+W5lBdf62DyPheURebiqXnmN30s+pRJh95qggnKkduQKBgGTFgrGu\nmv8IulJt74otFhiuA203WL3ZAMArp0L7QOZwVbh35f+7YrGtgBDTjlV0AEu1WxTu\nV3p+ZdDWP6rLkxnorSe1h8OQwQ+O/cuvTpXITivrvXHksfFu4s2anRnpdtvUErBr\nLHiOT692BqRzZWE0Qq9X1suVKni3Mc5EWJC9AoGBALTO/skNub2UypjatNZVTyjp\n8fau8992yv/9NPPiF/UJ9UL91zdIVgwbUxA+gacwjBKYSfXaj/GeQlhvknFV4ntU\nPxuawF6intK+cScB5dwxvoTiDNZCtqkiw5w/HbCRzf5mZT3QLn+reg4ViQrkCl1J\nWJxCL9gfLW0OgAs20EYc\n-----END PRIVATE KEY-----\n",
-      "client_email": "oscar-phone@homy-3693e.iam.gserviceaccount.com",
-      "client_id": "113236149207862146564",
-      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-      "token_uri": "https://oauth2.googleapis.com/token",
-      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/oscar-phone%40homy-3693e.iam.gserviceaccount.com",
-      "universe_domain": "googleapis.com"
+      "project_id": dotenv.env['PROJECT_ID'],
+      "private_key_id": dotenv.env['PRIVATE_KEY_ID'],
+      "private_key": dotenv.env['PRIVATE_KEY'],
+      "client_email": dotenv.env['CLIENT_EMAIL'],
+      "client_id": dotenv.env['CLIENT_ID'],
+      "auth_uri": dotenv.env['AUTH_URI'],
+      "token_uri": dotenv.env['TOKEN_URI'],
     };
 
     List<String> scopes = [
